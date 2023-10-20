ed67f31b51ec322b9298167b1c149a73321a2e20
teamkweku <teamkweku@outlook.com>
2023-10-20 11:04:25 +0000

    pycodestyle fix

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,8 +7,10 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello():
+    """method for the hello route"""
     return "Hello HBNB"
 
 
 if __name__ == "__main__":
+    """start the flask app on port 50000 for all hosts"""
     app.run(host="0.0.0.0", port=5000)
